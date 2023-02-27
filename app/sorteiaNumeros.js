d91ffe4b8810c7ab75ec40e12fea8b02e7cb2967
Ana Flavia de Sousa Monteiro <107011686+anafsmonteiro@users.noreply.github.com>
2023-02-27 08:26:34 -0300

    publicando o jogo

---
+++ app/sorteiaNumeros.js
@@ -0,0 +1,15 @@
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gertarNumeroAleatorio();
+
+function gertarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log(`Número secreto ${numeroSecreto}`);
+
+const elementoMenorValor = document.querySelector('#menor-valor').innerHTML = menorValor;
+
+const elementoMaiorValor = document.querySelector('#maior-valor').innerHTML = maiorValor;
+
+//Web Speech API - consultar MDN
